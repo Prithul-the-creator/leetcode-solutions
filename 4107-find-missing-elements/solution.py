c0cf4127e2772430d650e2eedf42378ed2f265df
Prithul-the-creator <themagicman932@gmail.com>
2026-08-04 17:04:38 +0000

    [LeetCode Sync] Runtime - 3 ms (60.75%), Memory - 19.3 MB (61.56%)

---
+++ 4107-find-missing-elements/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        
+
+
+        minimum = min(nums)
+        maximum = max(nums)
+        nums = set(nums)
+        result = []
+
+        for i in range(minimum, maximum + 1):
+
+            if i not in nums:
+                result.append(i)
+                
+        return result
+
+
+
+
