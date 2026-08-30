e7c8de647c2a661eea092ca212787b04cd268f9c
Prithul-the-creator <themagicman932@gmail.com>
2026-08-30 17:15:34 +0000

    [LeetCode Sync] Runtime - 19 ms (27.15%), Memory - 19.8 MB (34.84%)

---
+++ 0051-n-queens/solution.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+
+        result = []
+        board = [["."]*n for i in range(n)]
+        rows, cols, posdiag, negdiag = set(), set(), set(), set()
+        def dfs(i, j, queens):
+            nonlocal result
+            if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or i in rows or j in cols or i-j in posdiag or i + j in negdiag:
+                return
+            
+
+            board[i][j] = "Q"
+            if queens == n:
+                result.append(["".join(row) for row in board])
+                board[i][j] = "."
+                return
+            rows.add(i)
+            cols.add(j)
+            posdiag.add(i - j)
+            negdiag.add(i + j)
+
+            for down in range(n):
+                dfs(down, j + 1, queens + 1)
+
+            board[i][j] = "."
+            rows.remove(i)
+            cols.remove(j)
+            posdiag.remove(i - j)
+            negdiag.remove(i + j)
+        
+        for row in range(n):
+            dfs(row, 0, 1)
+        return result
+        
