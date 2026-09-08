0a2bb189a9ba60b68ad17123ba285c9ca6b96a62
Prithul-the-creator <themagicman932@gmail.com>
2026-09-08 05:27:30 +0000

    [LeetCode Sync] Runtime - 25 ms (12.43%), Memory - 21.7 MB (97.38%)

--- 0485-max-consecutive-ones/solution.py
+++ 0485-max-consecutive-ones/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
 
+            current = 0
+            result = 0
+            for j in range(len(nums)):
 
-            def solution(l, r):
-
-                if l - r == 0:
-                    return nums[l], nums[l], nums[l]
+                if nums[j] == 1:
+                    current += 1
+                    result = max(result, current)
                 
-                mid = l + (r - l)//2
-                ll, lr, lmax = solution(l, mid)
-                rl, rr, rmax = solution(mid + 1, r)
-
-                if ll == mid - l + 1: ll += rl
-                if rr == r - (mid + 1) + 1: rr += lr
-                return ll, rr, max(lr + rl, lmax, rmax)
+                else:
+                    current = 0
             
-            return solution(0, len(nums) - 1)[2]
+            return result
+
+
